363494cba754c533de688f112631dfbd8fdab3fc
Sabbath-.80 <miriamg.rg80@gmail.com>
2019-11-14 12:54:19 -0600

    LectorFeedApp

---
+++ app/src/main/java/mx/edu/utng/lectorfeedapp/RSSItem.java
@@ -0,0 +1,18 @@
+package mx.edu.utng.lectorfeedapp;
+
+public class RSSItem {
+
+    public String title;
+    public String link;
+    public String description;
+    public String pubdate;
+    public String guid;
+
+    public RSSItem(String title, String link, String description, String pubdate, String guid) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubdate = pubdate;
+        this.guid = guid;
+    }
+}
